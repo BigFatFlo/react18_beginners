4d6c8722e2613788e87035cd64f31630ed26be40
Florent Remis <florent.remis@gmail.com>
2024-12-04 22:39:06 +0100

    Expense tracker

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -7,8 +7,21 @@ import Like from "./components/Like";
 import produce from "immer";
 import ExpandableText from "./components/ExpandableText";
 import Form from "./components/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+import categories from "./expense-tracker/categories";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ]);
+
   const [bugs, setBugs] = useState([
     { id: 1, title: "Bug 1", fixed: false },
     { id: 2, title: "Bug 2", fixed: false },
@@ -34,6 +47,10 @@ function App() {
       })
     );
   };
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
 
   return (
     <>
@@ -67,6 +84,22 @@ function App() {
         Here is a very long text that should be shortened for visibility
       </ExpandableText>
       <Form />
+      <div className="mb-5">
+        <ExpenseForm
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        ></ExpenseForm>
+      </div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        ></ExpenseFilter>
+      </div>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
     </>
   );
 }
